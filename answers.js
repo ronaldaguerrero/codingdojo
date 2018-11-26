8577990350f0a9c951240bfb25ab9627168f949d
ronaldaguerrero <44407458+ronaldaguerrero@users.noreply.github.com>
2018-11-26 09:16:42 -0800

    Assignment #1

---
+++ answers.js
@@ -0,0 +1,17 @@
+/* Setting and Swapping */
+var myNumber = 42
+var myName = 'Ron'
+var myNumber = myName
+var MyName = myNumber 
+
+/* Print -52 to 1066 */
+var arr = [];
+for (var i = -52; i < 1067; i++){
+  arr.push(i){
+    console.log(i);
+    
+/* Don't Worry, Be Happy */
+var beCheerful() = 'good morning!'
+console.log(beCheerful()*98)
+
+/* Multiples of Three -- but Not All */
