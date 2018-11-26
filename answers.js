98d67669fa79323fe0011c9230e32c354863a9b4
ronaldaguerrero <44407458+ronaldaguerrero@users.noreply.github.com>
2018-11-26 10:26:44 -0800

    Update answers.js

--- answers.js
+++ answers.js
@@ -5,13 +5,10 @@ var myNumber = myName
 var MyName = myNumber 
 
 /* Print -52 to 1066 */
-function get_array(){
-  var arr = [];
-  for (var i = -52; i < 1067; i++){
-    arr.push(i)
-  }
+for (let i = -52; i < 1067; i++){
     console.log(i);
-  }
+
+
     
 /* Don't Worry, Be Happy */
 var beCheerful = 'good morning!'
@@ -19,7 +16,7 @@ console.log(beCheerful.repeat(98));
 
 /* Multiples of Three -- but Not All 
 need to skip -3 and -6 */
-for ( var i = -300 ; i < 1 ; i++ ) {
+for (let i = -300 ; i < 1 ; i++ ) {
     if(i % 3 === 0) {
         document.write(i + "<br>");
     }
