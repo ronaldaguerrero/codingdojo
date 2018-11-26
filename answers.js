128704c036e0d6cd4539a536286313e4120e468e
ronaldaguerrero <44407458+ronaldaguerrero@users.noreply.github.com>
2018-11-26 09:30:51 -0800

    Update answers.js

--- answers.js
+++ answers.js
@@ -5,13 +5,16 @@ var myNumber = myName
 var MyName = myNumber 
 
 /* Print -52 to 1066 */
-var arr = [];
-for (var i = -52; i < 1067; i++){
-  arr.push(i){
+function get_array(){
+  var arr = [];
+  for (var i = -52; i < 1067; i++){
+    arr.push(i)
+  }
     console.log(i);
+  }
     
 /* Don't Worry, Be Happy */
-var beCheerful() = 'good morning!'
-console.log(beCheerful()*98)
+var beCheerful = 'good morning!'
+console.log(beCheerful.repeat(98));
 
 /* Multiples of Three -- but Not All */
