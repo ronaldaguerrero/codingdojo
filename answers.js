30b1df74c670e75f42677e52955b3aee239bdbe0
ronaldaguerrero <44407458+ronaldaguerrero@users.noreply.github.com>
2018-11-26 15:31:26 -0800

    Update answers.js

--- answers.js
+++ answers.js
@@ -5,7 +5,7 @@ var myNumber = myName;
 var MyName = myNumber;
 
 /* Print -52 to 1066 */
-for (let i = -52; i < 1067; i++){
+for (var i = -52; i < 1067; i++){
     console.log(i);
     
 /* Don't Worry, Be Happy */
@@ -14,7 +14,7 @@ console.log(beCheerful.repeat(98));
 
 /* Multiples of Three -- but Not All 
 need to skip -3 and -6 */
-for (let i = -300 ; i < 1 ; i++ ) {
+for (var i = -300 ; i < 1 ; i++ ) {
     if(i === -6) {break; }{
         else
         (i % 3 === 0) {
@@ -36,22 +36,31 @@ for(var i = 2000; i <= 5280; i++) {
 /* Leap Year */
     
 /* Print and Count */
-for (let i = 512; i <= 4096; i++){
+for (var i = 512; i <= 4096; i++){
      if (i % 5 === 0){
     document.write(i + "<br>");
 }
 } 
     
 /* Multiples of Six */
+    var i = 0;
+while (i < 60,000) {
+  i++;
+  i += i;
     
 /* Counting, the Dojo Way */
-for (let i = 1; i <= 100;i++){
-    if (i % 5 === 0){
-        console.log('Coding'){
-            elseif (i % 10 === 0){
-                console.log(' Dojo')}
+for (var i = 1; i <= 100;i++){
+    if (i % 5 && i % 10 === 0){
+        console.log(i){
+            else{
+                if (i % 10 === 0){
+                    console.log(' Dojo');
+                }
+                if (i % 5 ===0){
+                    console.log('Coding');
         }
     }
+    console.log('<br>');
 }
  
     
