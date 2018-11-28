57eefd8ac23b01acde5f0fb90d60667917bedbce
ronaldaguerrero <44407458+ronaldaguerrero@users.noreply.github.com>
2018-11-27 16:21:52 -0800

    Update answers.js

--- answers.js
+++ answers.js
@@ -36,6 +36,14 @@ if (x === 11 || x === 15){
 } else {
   console.log('Just another day....');
 }
+
+var x = 11;
+x = window.prompt('Input a #: ');
+if (x = 11||15){
+  console.log('How did you know?');
+} else {
+  console.log('Just another day....');
+}
     
 /* Leap Year */
 year = window.prompt("Input a Year : ");
@@ -57,8 +65,19 @@ while (x <= 60000){
   x++;
 }
     
-/* Counting, the Dojo Way */
-
+/*[DONE] Counting, the Dojo Way */
+for (i=1; i<=100; i++) {
+    if(i % 5 ===0 && i % 10 === 0){
+        document.write(' Coding Dojo ');}
+    else if(i % 5 === 0){
+        document.write('Coding ');}
+    else if(i % 10 === 0){
+        document.write('Dojo ');}
+    else{
+        console.log(i);
+    }
+}
+       
  
     
 /*[working] Whoa, That Sucker's Huge... */
